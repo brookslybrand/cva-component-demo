688dc9ab2cf8ab014596b8ee69205c9c85f3780b
brookslybrand <brookslybrand@gmail.com>
2023-08-28 08:29:10 -0500

    Ignore any in types

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import type { VariantProps } from "class-variance-authority";
 import type { Simplify } from "type-fest";
 
 export type RequiredVariantProps<
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   T extends (...args: any) => any,
   // By default, all variants will be required
   Keys extends keyof VariantProps<T> = keyof VariantProps<T>
